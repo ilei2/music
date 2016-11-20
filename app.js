05c9c4f0a61d0fb99830232887dea4edb50d4001
ilei2 <ilei2@illinois.edu>
2016-11-20 01:57:25 -0600

    squares

--- app.js
+++ app.js
@@ -1,59 +1,66 @@
+//(function() {
 $(document).ready(function () {
+    'use strict';
 
-  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  var audioElement = document.getElementById('audioElement');
-  var audioSrc = audioCtx.createMediaElementSource(audioElement);
-  var analyser = audioCtx.createAnalyser();
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    var audioElement = document.getElementById('audioElement');
+    var audioSrc = audioCtx.createMediaElementSource(audioElement);
+    var analyser = audioCtx.createAnalyser();
 
-  // Bind our analyser to the media element source.
-  audioSrc.connect(analyser);
-  audioSrc.connect(audioCtx.destination);
+    // bind our analyser to the media element source.
+    audioSrc.connect(analyser);
+    audioSrc.connect(audioCtx.destination);
 
-  //var frequencyData = new Uint8Array(analyser.frequencyBinCount);
-  var frequencyData = new Uint8Array(200);
+    // var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+    var frequencyData = new Uint8Array(200);
 
-  var svgHeight = '300';
-  var svgWidth = '1200';
-  var barPadding = '1';
+    var svgHeight = 600,
+        svgWidth = 960;
 
-  function createSvg(parent, height, width) {
-    return d3.select(parent).append('svg').attr('height', height).attr('width', width);
-  }
-
-  var svg = createSvg('body', svgHeight, svgWidth);
-
-  // Create our initial D3 chart.
-  svg.selectAll('rect')
-     .data(frequencyData)
-     .enter()
-     .append('rect')
-     .attr('x', function (d, i) {
-        return i * (svgWidth / frequencyData.length);
-     })
-     .attr('width', svgWidth / frequencyData.length - barPadding);
-
-  // Continuously loop and update chart with frequency data.
-  function renderChart() {
-     requestAnimationFrame(renderChart);
-
-     // Copy frequency data to frequencyData array.
-     analyser.getByteFrequencyData(frequencyData);
-
-     // Update d3 chart with new data.
-     svg.selectAll('rect')
-        .data(frequencyData)
-        .attr('y', function(d) {
-           return svgHeight - d;
-        })
-        .attr('height', function(d) {
-           return d;
-        })
-        .attr('fill', function(d) {
-           return 'rgb(0, 0, ' + d + ')';
+    var svg = d3.select('body').append('svg')
+        .attr({
+            height: svgHeight,
+            width: svgWidth
         });
-  }
 
-  // Run the loop
-  renderChart();
+    // continuously loop and update chart with frequency data.
+    function renderChart() {
+        requestAnimationFrame(renderChart);
 
-});
+        // copy frequency data to frequencyData array.
+        analyser.getByteFrequencyData(frequencyData);
+        // console.log(frequencyData);
+
+
+        var length = d3.scale.linear()
+                  .domain([0, d3.max(frequencyData)])
+                  .range([0, svgHeight/3+10]);
+        var hueScale = d3.scale.linear()
+                  .domain([0, d3.max(frequencyData)])
+                  .range([0, 200]); //360
+        var squares = svg.selectAll('rect')
+                  .data(frequencyData);
+        squares.enter().append('rect');
+        squares
+            .attr({
+              x: function(d) {return 500-length(d);},
+              y: function(d) {return 300-length(d);},
+              width: function(d) {return length(d)*2;},
+              height: function(d) {return length(d)*2;},
+              fill: 'none',
+              'stroke-width': 2,
+              'stroke-opacity': 0.4,
+              stroke: function(d) { return d3.hsl(hueScale(d), 1, .5);}
+            });
+
+        // scale things to fit
+        squares.exit().remove();
+        //circles.exit().remove();
+    }
+
+    // run the loop
+    renderChart();
+    // just for blocks viewer size
+    d3.select(self.frameElement).style('height', '700px');
+
+}());
